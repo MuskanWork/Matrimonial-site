28986cd480bb3df33cfc86019fda11dbfbecf9c3
MuskanWork <mudani@bestpeers.com>
2021-03-16 21:17:03 +0530

    header footer and register page in process

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Foot from './component/footer/footer';
+import Nav1 from './component/navbar/Nav1';
+import Register from './component/navbar/Register';
+
+function App() {
+  return (
+    <>
+    <Nav1 />
+  
+    <Foot />
+   </>
+  );
+}
+
+export default App;
---
+++ src/component/navbar/Nav1.js
@@ -0,0 +1,35 @@
+import { Navbar, NavDropdown, Nav, Button, Form, FormControl } from 'react-bootstrap';
+import logo1 from '/home/rails/Desktop/Project/shadimubarak/src/images/logo1.jpg';
+import '/home/rails/Desktop/Project/shadimubarak/src/component/navbar/navstyle.css';
+import Register from '/home/rails/Desktop/Project/shadimubarak/src/component/navbar/Register.js';
+const Nav1 = (props) => {
+
+  return (
+    <>
+      <Navbar className="bg" expand="lg">
+        <Navbar.Brand href="#home"><img src={logo1} width="120" height="80" alt="" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#About">About</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <button class="btn btn-primary my-2 my-sm-0" type="submit">Login</button>
+            <Register onClick={props.handleClose} type="button" class="btn btn-primary my-2 my-sm-0"></Register>
+            
+
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  )
+}
+export default Nav1;
